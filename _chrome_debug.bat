3f4ebdd6003540833738f384ffee2eef63db3c6f
Flo Ledermann <ledermann@ims.tuwien.ac.at>
2017-02-13 12:04:10 +0100

    improving utility batch files

--- _chrome_debug.bat
+++ _chrome_debug.bat
@@ -1,2 +1,3 @@
-chrome.exe --user-data-dir="C:\Users\flo\AppData\Local\Google\Chrome\User Data DEBUG" --remote-debugging-port=9222
+echo %~dp0
+chrome.exe --user-data-dir="%~dp0_chrome_user_data_debug" --remote-debugging-port=9222
 pause
--- _serve.bat
+++ _serve.bat
@@ -1,2 +1,9 @@
-http-server testcases
+@http-server testcases
+@if %ERRORLEVEL% NEQ 0 (
+  @echo.
+  echo ####################################################################
+  echo # node.js webserver not found                                      #
+  echo # Please install node webserver using "npm install http-server -g" #
+  echo ####################################################################
+)
 pause
